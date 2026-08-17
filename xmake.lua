b5d497c2fbee6adfc51d96bc06cb848b1bcf37b6
sfdgdrfsrf <ryskeyseth@gmail.com>
2026-08-17 14:28:16 -0500

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -7,4 +7,8 @@ set_languages("c++20")
 target("Personalized")
     set_kind("shared")
     add_files("*.cpp")
+    
+    -- This points the compiler to the current folder for any path style
     add_includedirs(".")
+    add_includedirs("./Utils")
+    add_includedirs("./Hooks")
