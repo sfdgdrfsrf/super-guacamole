938c36c287a40df7f47b1ce9346b9c3b83018db8
sfdgdrfsrf <ryskeyseth@gmail.com>
2026-08-17 15:11:50 -0500

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,15 @@ set_version("1.0.0")
 
 set_languages("c++20")
 
+-- Automatically download the modern JSON library from the cloud
+add_requires("nlohmann_json")
+
 target("Personalized")
     set_kind("shared")
     add_files("*.cpp")
     
-    -- This forces the compiler to look in the flat folder and our custom external temp folder
+    -- Link the downloaded JSON package directly into your mod binary
+    add_packages("nlohmann_json")
+    
     add_includedirs(".")
     add_includedirs("/tmp/fake_ll")
